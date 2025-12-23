d06d4016610ceda6c0ff49f6d962cff8ab1b5a61
NS <enesenturk61@gmail.com>
2025-12-24 00:54:46 +0300

    init

---
+++ src/expenseTracker/ExpenseTracker.MobileApp/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseTracker.MobileApp
+{
+	public partial class AppShell : Shell
+	{
+
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+
+	}
+}
---
+++ src/expenseTracker/ExpenseTracker.MobileApp/Behaviors/ButtonAnimationBehavior.cs
@@ -0,0 +1,26 @@
+﻿namespace ExpenseTracker.MobileApp.Behaviors
+{
+	public class ButtonAnimationBehavior : Behavior<Button>
+	{
+		protected override void OnAttachedTo(Button bindable)
+		{
+			base.OnAttachedTo(bindable);
+			bindable.Clicked += OnButtonClicked;
+		}
+
+		protected override void OnDetachingFrom(Button bindable)
+		{
+			base.OnDetachingFrom(bindable);
+			bindable.Clicked -= OnButtonClicked;
+		}
+
+		private async void OnButtonClicked(object? sender, EventArgs e)
+		{
+			if (sender is not Button btn)
+				return;
+
+			await btn.ScaleTo(1.2, 100);
+			await btn.ScaleTo(1, 100);
+		}
+	}
+}
